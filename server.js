27531fdcebafa0265a524fe8024ef190bfb556ef
Jeremy Crane <jeremyccrane@me.com>
2024-05-29 15:59:27 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const prerender = require('prerender');
+const redisCache = require('prerender-redis-cache');
 
 const server = prerender({
   chromeLocation: '/usr/bin/google-chrome-stable',
@@ -15,6 +16,8 @@ const server = prerender({
   port: process.env.PORT || 3000
 });
 
+server.use(redisCache);
+
 server.use(prerender.sendPrerenderHeader());
 server.use(prerender.blockResources());
 server.use(prerender.removeScriptTags());
@@ -22,19 +25,19 @@ server.use(prerender.httpHeaders());
 server.use(prerender.addMetaTags());
 
 // Middleware to log requests
-server.use({
-  requestReceived: (req, res, next) => {
-    console.log(`Request received: ${req.prerender.url}`);
-    next();
-  },
-  pageLoaded: (req, res, next) => {
-    console.log(`Page loaded: ${req.prerender.url}`);
-    next();
-  },
-  failedRequest: (req, res, next) => {
-    console.error(`Request failed: ${req.prerender.url}`);
-    next();
-  }
-});
+// server.use({
+//   requestReceived: (req, res, next) => {
+//     console.log(`Request received: ${req.prerender.url}`);
+//     next();
+//   },
+//   pageLoaded: (req, res, next) => {
+//     console.log(`Page loaded: ${req.prerender.url}`);
+//     next();
+//   },
+//   failedRequest: (req, res, next) => {
+//     console.error(`Request failed: ${req.prerender.url}`);
+//     next();
+//   }
+// });
 
 server.start();
