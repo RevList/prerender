2c5e8d37199ebd9c907a64c196912fbfbe9709d5
Jeremy Crane <jeremyccrane@me.com>
2024-05-29 16:08:26 -0600

    Update server.js

--- server.js
+++ server.js
@@ -28,26 +28,33 @@ server.use(redisCache({
   expire: 60 * 60 * 24 // Cache expiration time in seconds (24 hours)
 }));
 
+// Middleware to add a delay and check for meta tags
+server.use({
+  requestReceived: (req, res, next) => {
+    console.log(`Request received: ${req.prerender.url}`);
+    next();
+  },
+  pageLoaded: (req, res, next) => {
+    setTimeout(() => {
+      next();
+    }, 5000); // Wait for 5 seconds
+  },
+  pageDoneCheck: (req, res) => {
+    const isPageDone = req.prerender.document.querySelector('meta[property="og:title"]') &&
+                       req.prerender.document.querySelector('meta[property="og:description"]') &&
+                       req.prerender.document.querySelector('meta[property="og:image"]');
+    return isPageDone;
+  },
+  failedRequest: (req, res, next) => {
+    console.error(`Request failed: ${req.prerender.url}`);
+    next();
+  }
+});
+
 server.use(prerender.sendPrerenderHeader());
 server.use(prerender.blockResources());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
 server.use(prerender.addMetaTags());
-
-// Middleware to log requests
-// server.use({
-//   requestReceived: (req, res, next) => {
-//     console.log(`Request received: ${req.prerender.url}`);
-//     next();
-//   },
-//   pageLoaded: (req, res, next) => {
-//     console.log(`Page loaded: ${req.prerender.url}`);
-//     next();
-//   },
-//   failedRequest: (req, res, next) => {
-//     console.error(`Request failed: ${req.prerender.url}`);
-//     next();
-//   }
-// });
 
 server.start();
