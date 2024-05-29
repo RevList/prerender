648050605b2776d179cc6adb82396ed5964dbc03
Jeremy Crane <jeremyccrane@me.com>
2024-05-29 15:43:59 -0600

    Updates

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const prerender = require('prerender');
 
 const server = prerender({
-  chromeLocation: '/usr/bin/google-chrome-stable', // Specify the correct path to Chrome
-  chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars'],
+  chromeLocation: '/usr/bin/google-chrome-stable',
+  chromeFlags: [
+    '--no-sandbox',
+    '--headless',
+    '--disable-gpu',
+    '--remote-debugging-port=9222',
+    '--hide-scrollbars',
+    '--disable-software-rasterizer'
+  ],
+  logRequests: true,
+  logErrors: true,
   port: process.env.PORT || 3000
 });
 
@@ -11,5 +20,13 @@ server.use(prerender.blockResources());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
 server.use(prerender.addMetaTags());
+
+server.on('pageLoaded', (req, res) => {
+  console.log(`Page loaded: ${req.prerender.url}`);
+});
+
+server.on('failedRequest', (req, res) => {
+  console.error(`Request failed: ${req.prerender.url}`);
+});
 
 server.start();
